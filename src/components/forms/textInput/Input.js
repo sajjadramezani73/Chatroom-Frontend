da0c549c1575c3c27008441985fa8c9d61fb45ac
sajjadramezani <sajjad.payam77@gmail.com>
2023-02-12 23:49:44 +0330

    fix: Change input component - part 01

--- src/components/forms/textInput/Input.js
+++ src/components/forms/textInput/Input.js
@@ -1,5 +1,5 @@
-
 import { useEffect, useState } from 'react';
+import useValidation from '../../../hooks/useValidation';
 import LoadSvgIcon from '../../../utils/LoadSvgIcon'
 
 const Input = ({
@@ -9,20 +9,33 @@ const Input = ({
     rule = "",
     value = "",
     onChange,
-    errorMessage = null,
+    disabled = false,
     attributes = {},
-    haveError = function () { },
-
+    haveError
 }) => {
 
-    const [focused, setFocused] = useState(false);
+    const { validate } = useValidation()
+
+    const [focusInput, setFocusInput] = useState(false);
     const [inputValue, setInputValue] = useState('')
-    const [error, setError] = useState(null)
-    const clearInputValue = () => onChangeHandler('')
+    const [validInput, setValidInput] = useState(null);
 
     useEffect(() => {
         setInputValue(value)
     }, [value])
+
+    useEffect(() => {
+        inputValue === '' && setValidInput(null)
+    }, [inputValue])
+
+    useEffect(() => {
+        if (haveError === true) {
+            setValidInput(false)
+        } else if (haveError === false) {
+            setValidInput(true)
+        }
+    }, [haveError])
+
 
     const onChangeHandler = (val) => {
         onChange({
@@ -32,62 +45,53 @@ const Input = ({
         })
     }
 
-    useEffect(() => {
-        if (value !== '' && value?.length >= 0) {
-            if (error) {
-                haveError(true)
-            } else {
-                haveError(false)
-            }
-        } else {
-            haveError(true)
-        }
-    }, [error, value])
+    const validHandler = () => {
+        setFocusInput(false)
+        const valid = validate({ rule: rule, value: inputValue })
+        setValidInput(valid.isValid)
+    }
 
     return (
-        <div className="py-2">
-            <div className={`flex h-11 rounded-xl border items-center
-            ${error != null ? 'border-danger' : focused ? 'border-gray-500' : (!!inputValue ? 'border-body' : '')}`}>
-                <div className='w-10 h-full flex justify-center items-center'>
-                    <LoadSvgIcon name={iconName} size="16" />
-                </div>
-                <div className='grow h-full relative text-right'>
+        <>
+            <div className={`inputBox relative flex items-center border border-light h-12 rounded-md 
+                ${disabled && 'opacity-60 bg-gray-50'}
+                ${validInput ? 'bg-gray-100' : 'bg-white'}
+                ${validInput === false ? 'border-danger' : ''}`}
+            >
+                <div className='flex-grow h-full relative'>
                     <input
                         type={type}
                         value={inputValue}
-                        onChange={val => onChangeHandler(val.target.value)}
-                        onFocus={() => setFocused(true)}
-                        onBlur={() => setFocused(false)}
-                        className="w-full h-full outline-none bg-transparent text-xxs text-body font-bold componentinput"
-                        placeholder={placeholder}
-                        {...attributes} />
+                        onChange={e => onChangeHandler(e.target.value)}
+                        onBlur={validHandler}
+                        onFocus={() => setFocusInput(true)}
+                        className="w-full h-full outline-none bg-transparent text-xs text-caption font-bold px-4"
+                        disabled={disabled}
+                        {...attributes}
+                    />
                 </div>
-                {value != '' ?
-                    <span className="px-2" onClick={clearInputValue}>
-                        <LoadSvgIcon name="close-circle" size={16} />
-                    </span>
-                    : null}
+                <p className={`placeholder absolute right-3 text-xs text-captionLight px-2 pointer-events-none transition-all duration-300
+                    ${(focusInput || inputValue !== '') ? '-top-2 bg-white text-[11px]' : 'top-1/2 -translate-y-1/2'}`}>
+                    {placeholder}
+                </p>
+                {validInput === true && (
+                    <div className="w-10 min-w-[40px] h-full flex items-center justify-center">
+                        <span className=''>
+                            <LoadSvgIcon name="check" size={18} weight={1.5} color="var(--color-primary)" />
+                        </span>
+                    </div>
+                )}
+                {validInput === false && (
+                    <div className="w-10 min-w-[40px] h-full flex items-center justify-center">
+                        <span className=''>
+                            <LoadSvgIcon name="close-circle" size={18} weight={1.5} color="var(--color-danger)" />
+                        </span>
+                    </div>
+                )}
             </div>
-            {error != null ?
-                <div className="w-full flex mt-2">
-                    <span className="px-2">
-                        <LoadSvgIcon name="info" color="var(--color-danger)" size={16} />
-                    </span>
-                    <span className="text-danger text-[10px]">{error}</span>
-                </div>
-                : null
-            }
-            <style>
-                {`
-                    .componentinput::placeholder {
-                        color:#c1c1c1;
-                        font-size:12px;
-                        font-weight:500;
-                    }
-                `}
-            </style>
-        </div>
+        </>
     );
 };
 
 export default Input;
+
