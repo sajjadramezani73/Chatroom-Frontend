232116162f9922ced4030d0c3b37e73a79335a8d
sajjadramezani <sajjad.payam77@gmail.com>
2023-02-15 12:55:02 +0330

    fix: Improve left sidebar in home page - part 01
    add exit icon and avatar img

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import { Outlet, useNavigate } from "react-router-dom";
 import Conversations from "../containers/conversations/Conversation";
 import { getIndex } from "../services/queries";
 import { addUser } from "../store/userSlice";
+import LoadSvgIcon from '../utils/LoadSvgIcon'
 
 const Home = () => {
 
@@ -29,7 +30,17 @@ const Home = () => {
     return (
         <div className="w-full h-full bg-captionLight flex items-stretch ">
             <div className="flex">
-                <div className="w-[100px] min-w-[100px] bg-grayExtraDark"></div>
+                <div className="w-[100px] min-w-[100px] bg-grayExtraDark">
+                    <div className="h-full flex flex-col justify-between">
+                        <div className="pt-20">
+                            <div className="w-16 h-16 rounded-full bg-red-200 mx-auto cursor-pointer"></div>
+                            <div className="mt-16"></div>
+                        </div>
+                        <span className="cursor-pointer flex justify-center py-6 hover:bg-grayDark duration-300">
+                            <LoadSvgIcon name="exit" color="var(--color-light)" />
+                        </span>
+                    </div>
+                </div>
                 <div className="w-[320px] min-w-[320px] bg-grayDark"></div>
                 {/* <Conversations /> */}
             </div>
