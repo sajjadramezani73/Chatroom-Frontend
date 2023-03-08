b3722227929596fbd491f50dcf0644a925cc13f4
sajjadramezani <sajjad.payam77@gmail.com>
2023-03-08 19:05:31 +0330

    refactor: Show dynamic component in sidebar home page
    show pvs and groups and contacts component with dynamic component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,11 @@ import Cookies from "js-cookie";
 import { lazy, Suspense, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Outlet, useNavigate } from "react-router-dom";
-import Conversations from "../containers/conversations/Conversation";
 import SidebarLinks from "../containers/sidebarLinks/SidebarLinks";
 import Avatar from "../containers/user/Avatar";
 import { getIndex } from "../services/queries";
 import { addUser } from "../store/userSlice";
 import LoadSvgIcon from '../utils/LoadSvgIcon'
-const Contects = lazy(() => import('../containers/contacts/Contacts'))
 
 const Home = () => {
 
@@ -17,7 +15,16 @@ const Home = () => {
     const { user } = useSelector(store => store.user);
     console.log('user', user);
 
-    const [ss, setSs] = useState(false);
+    const [selectedLink, setSelectedLink] = useState('pvs');
+
+    const ComponentLeader = (componentName) => {
+        if (componentName !== '') {
+            const Component = lazy(() => import('../containers/sidebar/' + componentName))
+            if (Component) return <Component />
+        } else {
+            return null
+        }
+    }
 
     useEffect(() => {
         if (user == null) {
@@ -41,13 +48,13 @@ const Home = () => {
 
     return (
         <div className="w-full h-full bg-captionLight flex items-stretch ">
-            <div className="flex">
+            <div className="flex h-full">
                 <div className="w-[100px] min-w-[100px] bg-grayExtraDark">
                     <div className="h-full flex flex-col justify-between">
                         <div className="pt-20">
                             <Avatar />
                             <div className="mt-16">
-                                <SidebarLinks changeLink={value => setSs(!ss)} />
+                                <SidebarLinks changeLink={value => setSelectedLink(value)} />
                             </div>
                         </div>
                         <span className="cursor-pointer flex justify-center py-6 hover:bg-grayDark duration-300"
@@ -56,10 +63,23 @@ const Home = () => {
                         </span>
                     </div>
                 </div>
-                <div className="w-[320px] min-w-[320px] bg-grayDark"></div>
-                <Suspense >
-                    {ss ? <Contects /> : <p>ssss</p>}
-                </Suspense>
+                <div className="w-[320px] min-w-[320px] bg-grayDark h-full flex flex-col">
+                    <div className="p-4 border-b border-b-grayExtraDark">
+                        <div className="flex items-center bg-grayExtraDark py-2 px-3 rounded-full rtl">
+                            <input
+                                type="text"
+                                placeholder="جستجو"
+                                className="w-full bg-transparent outline-none placeholder:text-light/50 text-light text-xs"
+                            />
+                            <LoadSvgIcon name="search" size={20} color="var(--color-light)" />
+                        </div>
+                    </div>
+                    <div className="p-4 flex-grow overflow-hidden overflow-y-auto no-scroll">
+                        <Suspense >
+                            {ComponentLeader(selectedLink)}
+                        </Suspense>
+                    </div>
+                </div>
             </div>
             <div className="bg-grayLight grow p-4">
                 <Outlet />
